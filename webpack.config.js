9d4fb0b012307b8f7bee2fbb6774d9e58d553463
Caleb <calebabratton@gmail.com>
2021-06-02 18:01:31 -0600

    Starting Point

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, 'client');
+const DIST_DIR = path.join(__dirname, 'public');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
