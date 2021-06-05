6662155a8381f38981f924862b8a154b92448eb1
Mikka Tully <mikka.tully@gmail.com>
2021-06-05 17:19:41 -0600

    Addressed esLint issues I made previously, inside apiReq file

--- helpers/apiRequest.js
+++ helpers/apiRequest.js
@@ -1,25 +1,24 @@
 const axios = require('axios');
 const config = require('../config.js');
 
-let sendAPIRequest = (addtlUrlPath, reqMethod, callback) => {
-  let options = {
+const sendAPIRequest = (addtlUrlPath, reqMethod, callback) => {
+  const options = {
     method: reqMethod,
     url: 'https://app-hrsei-api.herokuapp.com/api/fec2/HR-BLD17/' + addtlUrlPath,
     // url: 'https://api.github.com/users/' + username + '/repos',
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
-    }
+      'Authorization': `token ${config.TOKEN}`,
+    },
   };
 
   axios(options)
-    .then(function(response) {
-      callback(null, response);
-    })
-    .catch(function(err) {
-      callback(err, null);
-    })
-
-}
+      .then(function(response) {
+        callback(null, response);
+      })
+      .catch(function(err) {
+        callback(err, null);
+      });
+};
 
 module.exports.sendAPIRequest = sendAPIRequest;
