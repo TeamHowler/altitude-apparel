ac2047aa569d91025cb067f80b300943c8547612
Caleb <calebabratton@gmail.com>
2021-06-07 14:05:30 -0600

    product routes complete

--- client/ProductDetails/ProductDetails.jsx
+++ client/ProductDetails/ProductDetails.jsx
@@ -1,7 +1,12 @@
-// import React, { useState } from 'react';
+// import React, {useState} from 'react';
 
 // function ProductDetail() {
+//   const [product, updateProduct] = useState('');
 
+//   return (
+//     <div>hello {product}
+//       <button>{updateProduct}</button></div>
+//   );
 // }
 
 // export default ProductDetail;
--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const api = require('../helpers/sendAPIRequest.js');
 const app = express();
 const path = require('path');
 const port = 3000;
 const router = require('./routes.js');
-const products = require('./controllers/products.js');
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
 
 // Set up our routes
-app.use('/products', products);
+app.use('/products', router);
+// app.use('/products/reviews/', router);
 
+// app.use('/products/:product_id', router);
 // app.get('/products', (req, res) => {
 //   api.sendAPIRequest('/products', 'GET', (err, result) => {
 //     if (err) {
@@ -24,45 +24,47 @@ app.use('/products', products);
 //   });
 // });
 
-app.get('/products/:product_id', (req, res) => {
-  const id = req.params.product_id;
-  console.log('url in app.get====', req.params);
-  api.sendAPIRequest('/products/' + `${id}`, 'GET', (err, result) => {
-    if (err) {
-      console.log('err in app.get====', err);
-      res.send(err);
-    } else {
-      console.log('result in app.get===', result);
-      res.send(result);
-    }
-  });
-});
+// app.get('/products/:product_id', (req, res) => {
+//   const id = req.params.product_id;
+//   console.log('url in app.get====', req.params);
+//   api.sendAPIRequest('/products/' + `${id}`, 'GET', (err, result) => {
+//     if (err) {
+//       console.log('err in app.get====', err);
+//       res.send(err);
+//     } else {
+//       console.log('result in app.get===', result);
+//       res.send(result);
+//     }
+//   });
+// });
 
-app.get('/products/:product_id/styles', (req, res) => {
-  const id = req.params.product_id;
-  api.sendAPIRequest('/products/' + `${id}` + '/styles', 'GET', (err, result) => {
-    if (err) {
-      console.log('err in app.get====', err);
-      res.send(err);
-    } else {
-      console.log('result in app.get===', result);
-      res.send(result);
-    }
-  });
-});
+// app.get('/products/:product_id/styles', (req, res) => {
+//   const id = req.params.product_id;
+//   api.sendAPIRequest('/products/' + `${id}` + '/styles',
+//       'GET', (err, result) => {
+//         if (err) {
+//           console.log('err in app.get====', err);
+//           res.send(err);
+//         } else {
+//           console.log('result in app.get===', result);
+//           res.send(result);
+//         }
+//       });
+// });
 
-app.get('/products/:product_id/related', (req, res) => {
-  const id = req.params.product_id;
-  api.sendAPIRequest('/products/' + `${id}` + '/related', 'GET', (err, result) => {
-    if (err) {
-      console.log('err in app.get====', err);
-      res.send(err);
-    } else {
-      console.log('result in app.get===', result);
-      res.send(result);
-    }
-  });
-});
+// app.get('/products/:product_id/related', (req, res) => {
+//   const id = req.params.product_id;
+//   api.sendAPIRequest('/products/' + `${id}` + '/related', 'GET',
+// (err, result) => {
+//     if (err) {
+//       console.log('err in app.get====', err);
+//       res.send(err);
+//     } else {
+//       console.log('result in app.get===', result);
+//       res.send(result);
+//     }
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`listening to port ${port}`);
