960c19bfe6eae47c83e17d03be1dda770c3af80a
Caleb <calebabratton@gmail.com>
2021-06-08 11:30:53 -0600

    some working data and wireframe

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,15 @@
-
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import {ProductContext} from './context.js';
 import ProductDetails from './ProductDetails/ProductDetails.jsx';
+import {Nav, Navbar, NavDropdown, Container,
+  FormControl, Button, Form} from 'react-bootstrap';
 
 function App() {
-  const [products, updateProduct] = useState([]);
+  const [currentProduct, updateProduct] = useState([]);
 
   const fetch = () => {
-    axios.get('/products')
+    axios.get('/products/18080/')
         .then((response) => {
           updateProduct(response.data);
         })
@@ -24,8 +25,36 @@ function App() {
 
   return (
     <ProductContext.Provider value={{
-      products,
+      currentProduct,
     }}>
+      <Container>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Altitude Apparel</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search"
+                className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
       <ProductDetails />
     </ProductContext.Provider>
   );
