15dad41a76499e2b8b3e94686c7c3e4c0a5c4739
davidkharbin <david_k_harbin@hotmail.com>
2021-06-10 11:16:22 -0600

    added express.json() instead of depreciated bodyparser to server index.js

--- server/index.js
+++ server/index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
-const bodyParser = require('body-parser');
 const productRouter = require('./routes/productRouter.js');
 const reviewRouter = require('./routes/reviewRouter.js');
 const questionRouter = require('./routes/questionRouter.js');
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Set up our routes
 app.use('/products', productRouter);
