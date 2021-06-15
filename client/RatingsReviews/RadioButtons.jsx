803a2e1c4f71098aa20cbc15905d4ab65b9c22d2
Mikka Tully <mikka.tully@gmail.com>
2021-06-15 08:20:57 -0600

    Radio buttons appearing & selectable in 'add review' form and appropriately updating the characteristics object in state

--- client/RatingsReviews/RadioButtons.jsx
+++ client/RatingsReviews/RadioButtons.jsx
@@ -1,20 +1,19 @@
 import React, {useContext} from 'react';
-// import axios from 'axios';
 import {ProductContext} from '../context.js';
 import {Form} from 'react-bootstrap/';
 
 function RadioButtons({characteristic}) {
-  const {currentId, meta, newReview, updateNewReview} = useContext(ProductContext);
+  const {meta, newReview, updateNewReview} = useContext(ProductContext);
 
   function changeHandler(event) {
-    console.log('radio button id===', event.target.name);
-    console.log('radio button label====', typeof parseInt(event.target.value));
-    // updateNewReview({...newReview,
-    // `characteristics[event.target.name]: event.target.value`});
+    // create shallow copy of obj storage
+    const newCharacteristics = {...newReview.characteristics};
+    // re-assign new copy's values to form fields
+    newCharacteristics[event.target.name] = event.target.value;
+    // merge this new copy with current state's characteristics obj
+    updateNewReview({...newReview,
+      'characteristics': newCharacteristics});
   };
-
-  // console.log('characteristic inside radio buttons',
-  // meta['characteristics'][characteristic]);
 
   return (
     <div>
@@ -22,26 +21,28 @@ function RadioButtons({characteristic}) {
       {/* <Form.Check required onChange={changeHandler} > */}
       {/* <Form.Check.Input type='radio' isInvalid /> */}
       {/* <Form.Check.Label>{characteristic}</Form.Check.Label> */}
-      <Form.Check
-        inline label="1"
-        name={meta['characteristics'][characteristic].id}
-        type='radio' onChange={changeHandler} value='1' />
-      <Form.Check
-        inline label="2"
-        name={meta['characteristics'][characteristic].id}
-        type='radio' onChange={changeHandler} value='2' />
-      <Form.Check
-        inline label="3"
-        name={meta['characteristics'][characteristic].id}
-        type='radio' onChange={changeHandler} value='3' />
-      <Form.Check
-        inline label="4"
-        name={meta['characteristics'][characteristic].id}
-        type='radio' onChange={changeHandler} value='4' />
-      <Form.Check
-        inline label="5"
-        name={meta['characteristics'][characteristic].id}
-        type='radio' onChange={changeHandler} value='5' />
+      <Form required >
+        <Form.Check
+          inline label="1"
+          name={meta['characteristics'][characteristic].id}
+          type='radio' onChange={changeHandler} value='1' />
+        <Form.Check
+          inline label="2"
+          name={meta['characteristics'][characteristic].id}
+          type='radio' onChange={changeHandler} value='2' />
+        <Form.Check
+          inline label="3"
+          name={meta['characteristics'][characteristic].id}
+          type='radio' onChange={changeHandler} value='3' />
+        <Form.Check
+          inline label="4"
+          name={meta['characteristics'][characteristic].id}
+          type='radio' onChange={changeHandler} value='4' />
+        <Form.Check
+          inline label="5"
+          name={meta['characteristics'][characteristic].id}
+          type='radio' onChange={changeHandler} value='5' />
+      </Form>
       {/* <Form.Control.Feedback type="valid">
       You did it!</Form.Control.Feedback> */}
       {/* </Form.Check> */}
